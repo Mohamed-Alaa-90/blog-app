d2589c0920d2e4d85a66cb3d328529142258d7a2
Mohamed_Alaa <74032994+Mohamed-Alaa-90@users.noreply.github.com>
2026-08-12 11:12:59 +0300

    prepare API for production

--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,33 @@ use App\Http\Controllers\Api\ImageController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    // Authentication
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
 
-    // Posts
-    Route::get('/posts', [PostController::class, 'index']);
-    Route::get('/posts/{post}', [PostController::class, 'show']);
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::put('/posts/{post}', [PostController::class, 'update']);
-    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+Route::prefix("v1")->group(function () {
+    Route::middleware('throttle:auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+    Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+        // Authentication
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
 
-    // Comments
-    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
-    Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
-    Route::put('/posts/{post}/comments/{comment}', [CommentController::class, 'update'])->scopeBindings();
-    Route::delete('/posts/{post}/comments/{comment}', [CommentController::class, 'destroy'])->scopeBindings();
+        // Posts
+        Route::get('/posts', [PostController::class, 'index']);
+        Route::get('/posts/{post}', [PostController::class, 'show']);
+        Route::post('/posts', [PostController::class, 'store']);
+        Route::put('/posts/{post}', [PostController::class, 'update']);
+        Route::delete('/posts/{post}', [PostController::class, 'destroy']);
 
-    //image
-    Route::post('/posts/{post}/images', [ImageController::class, 'store']);
-    Route::delete('/posts/{post}/images/{image}', [ImageController::class, 'destroy'])->scopeBindings();
+        // Comments
+        Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+        Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
+        Route::put('/posts/{post}/comments/{comment}', [CommentController::class, 'update'])->scopeBindings();
+        Route::delete('/posts/{post}/comments/{comment}', [CommentController::class, 'destroy'])->scopeBindings();
+    });
+    Route::middleware(['auth:sanctum', 'throttle:images'])->group(function () {
+
+        Route::post('/posts/{post}/images', [ImageController::class, 'store']);
+        Route::delete('/posts/{post}/images/{image}', [ImageController::class, 'destroy'])->scopeBindings();
+    });
 });
